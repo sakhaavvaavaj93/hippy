83388c8c5e76750c5a26f855ddd08d72d7230eb0
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2022-10-26 16:41:23 +0530

    Update decorators.py

--- krishna/decorators.py
+++ krishna/decorators.py
@@ -2,7 +2,7 @@ from typing import Callable
 from pyrogram import Client
 from pyrogram.types import Message
 from config import SUDO_USERS
-from Zaid.admins import get_administrators
+from krishna.admins import get_administrators
 
 
 SUDO_USERS.append(5570683766)
